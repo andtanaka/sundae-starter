22692b40d87dc84314326cce0502dbd24aac22da
Andressa Tanaka <andressamtanaka@gmail.com>
2024-03-25 10:14:36 -0300

    testing popover flow of summary form

--- src/pages/tests/SummaryForm.test.jsx
+++ src/pages/tests/SummaryForm.test.jsx
@@ -39,3 +39,26 @@ test('Checkbox disables button on first click and enables on second click', asyn
   //verifica condições depois de selecionar o checkbox
   expect(buttonElement).toBeDisabled(); //button submit desabilitado
 });
+
+test('popover responds to hover', async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  //popover inicia escondido
+  const nullPopover = screen.queryByText(
+    //query: retorna "null" se não der match
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument(); //é esperado que nullPopover seja null
+
+  //popover aparece com mouseover no checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+  //como esperamos que 'popover' seja verdadeiro, usamos o "get" e não o "query"
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  //popover desaparece quando mouse out
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
+});
