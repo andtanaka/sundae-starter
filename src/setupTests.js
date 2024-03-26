d9dba5fbc4c13b67acde1b20f8db09232106067b
Andressa Tanaka <andressamtanaka@gmail.com>
2024-03-26 19:06:43 -0300

    setup mock service

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,13 @@
 import '@testing-library/jest-dom';
+import { beforeAll, afterEach, afterAll } from 'vitest';
+import { server } from './mocks/server';
+
+//Está estabelecendo a API mocking, antes de todos os testes
+beforeAll(() => server.listen());
+
+//Reseta qualquer request handlers que você possa adicionar durante os tests,
+//Assim, eles não irão afetar outros tests
+afterEach(() => server.resetHandlers());
+
+//Encerra o servidor e coloca em ordem depois que os tests forem finalizados
+afterAll(() => server.close());
